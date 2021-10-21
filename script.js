ad2b617f0271f4d0e7b1f93489603edf3e96cef9
Johan <joha034p@stud.kea.dk>
2021-10-21 13:13:44 +0200

    projects added and styled

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("start");
+  addEventListeners();
+}
+
+function addEventListeners() {
+  document.querySelectorAll(".project").forEach((element) => {
+    element.addEventListener("mouseover", projectHover);
+    element.addEventListener("mouseout", projectMouseout);
+  });
+}
+
+function projectHover() {
+  document.querySelector(`#${this.id} .txt h4`).classList.add("hide");
+  document.querySelector(`#${this.id} .read`).classList.add("show");
+}
+
+function projectMouseout() {
+  document.querySelector(`#${this.id} .txt h4`).classList.remove("hide");
+  document.querySelector(`#${this.id} .read`).classList.remove("show");
+}
