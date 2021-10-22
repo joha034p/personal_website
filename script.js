f533bdefa3308ff3145d0a6761835fb4c12a5a22
Johan <joha034p@stud.kea.dk>
2021-10-22 13:22:10 +0200

    projects cant close

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function addEventListeners() {
   document.querySelectorAll(".project").forEach((element) => {
     element.addEventListener("mouseover", projectHover);
     element.addEventListener("mouseout", projectMouseout);
+    element.addEventListener("mousedown", projectClick);
+  });
+  document.querySelectorAll(".project_info .x").forEach((element) => {
+    element.addEventListener("mousedown", closeProject);
   });
 }
 
@@ -21,4 +25,20 @@ function projectHover() {
 function projectMouseout() {
   document.querySelector(`#${this.id} .txt h4`).classList.remove("hide");
   document.querySelector(`#${this.id} .read`).classList.remove("show");
+}
+
+function projectClick() {
+  document.querySelector(`#${this.id} .txt h4`).classList.remove("hide");
+  document.querySelector(`#${this.id} .read`).classList.remove("show");
+  //   document.querySelector(`.${this.className}`).removeEventListener("mouseover", projectHover);
+  //   document.querySelector(`.${this.className}`).removeEventListener("mouseout", projectMouseout);
+  document.querySelector(`#${this.id} .project_info`).classList.remove("hide");
+  addEventListeners();
+}
+
+function closeProject() {
+  //   document.querySelector(`.${this.parentElement.className}`).removeEventListener("mousedown", projectClick);
+  console.log(this.parentElement);
+  this.parentElement.classList.add("hide");
+  addEventListeners();
 }
