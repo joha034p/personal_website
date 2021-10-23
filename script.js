f9958ffc92d1bee700dada61205a680e7958c127
Johan <joha034p@stud.kea.dk>
2021-10-23 19:05:21 +0200

    portrait added, styling changed

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ function start() {
 function addEventListeners() {
   document.querySelectorAll(".project").forEach((element) => {
     element.addEventListener("mouseover", projectHover);
-    element.addEventListener("mouseout", projectMouseout);
-    element.addEventListener("mousedown", projectClick);
   });
   document.querySelectorAll(".project_info .x").forEach((element) => {
     element.addEventListener("mousedown", closeProject);
@@ -18,27 +16,33 @@ function addEventListeners() {
 }
 
 function projectHover() {
+  this.removeEventListener("mouseover", projectHover);
+  this.addEventListener("mouseout", projectMouseout);
+  this.addEventListener("mousedown", projectClick);
   document.querySelector(`#${this.id} .txt h4`).classList.add("hide");
   document.querySelector(`#${this.id} .read`).classList.add("show");
 }
 
 function projectMouseout() {
+  this.removeEventListener("mouseout", projectMouseout);
+  this.addEventListener("mouseover", projectHover);
   document.querySelector(`#${this.id} .txt h4`).classList.remove("hide");
   document.querySelector(`#${this.id} .read`).classList.remove("show");
 }
 
 function projectClick() {
-  document.querySelector(`#${this.id} .txt h4`).classList.remove("hide");
-  document.querySelector(`#${this.id} .read`).classList.remove("show");
-  //   document.querySelector(`.${this.className}`).removeEventListener("mouseover", projectHover);
-  //   document.querySelector(`.${this.className}`).removeEventListener("mouseout", projectMouseout);
+  this.removeEventListener("mousedown", projectClick);
+  this.removeEventListener("mouseout", projectMouseout);
+  document.querySelector(`#${this.id} .project_info .x`).addEventListener("mousedown", closeProject);
+  document.querySelector(`#${this.id} .project_info`).classList.add("show");
   document.querySelector(`#${this.id} .project_info`).classList.remove("hide");
-  addEventListeners();
 }
 
 function closeProject() {
-  //   document.querySelector(`.${this.parentElement.className}`).removeEventListener("mousedown", projectClick);
-  console.log(this.parentElement);
+  this.removeEventListener("mousedown", closeProject);
+  this.parentElement.removeEventListener("mouseover", projectHover);
+  this.parentElement.removeEventListener("mouseout", projectMouseout);
   this.parentElement.classList.add("hide");
+  this.parentElement.classList.remove("show");
   addEventListeners();
 }
